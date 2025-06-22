d68ee5a91e52486e59c42d370bf31cb3bad48cb6
Diego <putindeergaming@gmail.com>
2025-06-22 06:36:12 -0400

    Adding documentation to Scoreboards.java

--- src/main/java/us/polarismc/polarisduels/utils/Scoreboards.java
+++ src/main/java/us/polarismc/polarisduels/utils/Scoreboards.java
@@ -5,8 +5,19 @@ import net.kyori.adventure.text.Component;
 import us.polarismc.polarisduels.Main;
 import us.polarismc.polarisduels.player.DuelsPlayer;
 
+/**
+ * Handles the creation and updating of scoreboards for players.
+ * This class provides utility methods for managing scoreboard content
+ * and tracking player statistics like online count, players in queue, and dueling players.
+ */
 public class Scoreboards {
     private static final Main plugin = Main.getInstance();
+    /**
+     * Updates the content of a player's scoreboard with current server statistics.
+     * Displays online player count, players in duels, players in queue, and ping.
+     *
+     * @param board The FastBoard instance to update
+     */
     public static void updateBoard(FastBoard board) {
         Component headfooter = plugin.utils.chat("&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r&7&m &r");
 
@@ -22,12 +33,22 @@ public class Scoreboards {
         );
     }
 
+    /**
+     * Counts the number of players currently in queue.
+     *
+     * @return The number of players in queue
+     */
     public static Integer queueCount() {
         return (int) plugin.getPlayerManager().getPlayerList().stream()
                 .filter(DuelsPlayer::isQueue)
                 .count();
     }
 
+    /**
+     * Counts the number of players currently in a duel or starting a duel.
+     *
+     * @return The number of players currently dueling or about to duel
+     */
     public static Integer duelingCount() {
         return (int) plugin.getPlayerManager().getPlayerList().stream()
                 .filter(p -> p.isDuel() || p.isStartingDuel())
