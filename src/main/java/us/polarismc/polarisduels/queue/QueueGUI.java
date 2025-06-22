282b2ac08bdac4ab8c564cfc3e2094b840c41749
Diego <putindeergaming@gmail.com>
2025-06-22 06:36:26 -0400

    Refactoring to use new API methods

--- src/main/java/us/polarismc/polarisduels/queue/QueueGUI.java
+++ src/main/java/us/polarismc/polarisduels/queue/QueueGUI.java
@@ -16,22 +16,22 @@ public class QueueGUI extends FastInv {
         super(owner -> Bukkit.createInventory(owner, 27, plugin.utils.chat("&cQueue")));
 
         int[] glass = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 18, 19, 20, 21, 22, 23, 24, 25, 26};
-        setItems(glass, new ItemBuilder(Material.BLUE_STAINED_GLASS_PANE).name("").hideAll().build());
+        setItems(glass, plugin.utils.ib(Material.BLUE_STAINED_GLASS_PANE).name("").hideTooltip().build());
 
         // SMP
-        setItem(10, new ItemBuilder(Material.ENDER_PEARL).name("&#ff00d1&lSMP").lore("&7Placeholder Lore for SMP").hideAll().build(), e -> setQueue(p, teamsize, plugin, KitType.SMP));
+        setItem(10, plugin.utils.ib(Material.ENDER_PEARL).name("&#ff00d1&lSMP").lore("&7Placeholder Lore for SMP").hideGUI().build(), e -> setQueue(p, teamsize, plugin, KitType.SMP));
         // AXE
-        setItem(11, new ItemBuilder(Material.DIAMOND_AXE).name("&#9300ff&lAxe").lore("&7Placeholder Lore for AXE").hideAll().build(), e -> setQueue(p, teamsize, plugin, KitType.AXE));
+        setItem(11, plugin.utils.ib(Material.DIAMOND_AXE).name("&#9300ff&lAxe").lore("&7Placeholder Lore for AXE").hideGUI().build(), e -> setQueue(p, teamsize, plugin, KitType.AXE));
         // NETHERITEPOT
-        setItem(12, new ItemBuilder(Material.NETHERITE_HELMET).name("&#808080&lNetheritePot").lore("&7Placeholder Lore for NetheritePot").hideAll().build(), e -> setQueue(p, teamsize, plugin, KitType.NETHPOT));
+        setItem(12, plugin.utils.ib(Material.NETHERITE_HELMET).name("&#808080&lNetheritePot").lore("&7Placeholder Lore for NetheritePot").hideGUI().build(), e -> setQueue(p, teamsize, plugin, KitType.NETHPOT));
         // UHC
-        setItem(13, new ItemBuilder(Material.GOLDEN_APPLE).name("&#d1bb00&lUHC").lore("&7Placeholder Lore for UHC").hideAll().build(), e -> setQueue(p, teamsize, plugin, KitType.UHC));
+        setItem(13, plugin.utils.ib(Material.GOLDEN_APPLE).name("&#d1bb00&lUHC").lore("&7Placeholder Lore for UHC").hideGUI().build(), e -> setQueue(p, teamsize, plugin, KitType.UHC));
         // DIAMONDPOT
-        setItem(14, new ItemBuilder(Material.SPLASH_POTION).potionType(PotionType.HEALING).name("&#ff0000&lDiamondPot").lore("&7Placeholder Lore for DiamondPot").hideAll().build(), e -> setQueue(p, teamsize, plugin, KitType.DIAMONDPOT));
+        setItem(14, plugin.utils.ib(Material.SPLASH_POTION).potionType(PotionType.HEALING).customName("&#ff0000&lDiamondPot").lore("&7Placeholder Lore for DiamondPot").hideGUI().build(), e -> setQueue(p, teamsize, plugin, KitType.DIAMONDPOT));
         // SWORD
-        setItem(15, new ItemBuilder(Material.DIAMOND_SWORD).name("&#00fff3&lSword").lore("&7Placeholder Lore for Sword").hideAll().build(), e -> setQueue(p, teamsize, plugin, KitType.SWORD));
+        setItem(15, plugin.utils.ib(Material.DIAMOND_SWORD).name("&#00fff3&lSword").lore("&7Placeholder Lore for Sword").hideGUI().build(), e -> setQueue(p, teamsize, plugin, KitType.SWORD));
         // CUSTOM
-        setItem(16, new ItemBuilder(Material.ENDER_EYE).name("&#04cd14&lCustom").lore("&cComing soon...").hideAll().build(), e -> setQueue(p, teamsize, plugin, KitType.CUSTOM));
+        setItem(16, plugin.utils.ib(Material.ENDER_EYE).name("&#04cd14&lCustom").lore("&cComing soon...").hideGUI().build(), e -> setQueue(p, teamsize, plugin, KitType.CUSTOM));
 
         addClickHandler(e -> e.setCancelled(true));
         open(p);
