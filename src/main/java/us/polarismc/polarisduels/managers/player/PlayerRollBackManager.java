108bf218d6c65c18ec66225a3c1184a93137fd5d
Diego <putindeergaming@gmail.com>
2026-03-04 23:52:31 -0300

    Refactor player rollback logic by delegating state reset and teleportation to `HubManager`.

--- src/main/java/us/polarismc/polarisduels/managers/player/PlayerRollBackManager.java
+++ src/main/java/us/polarismc/polarisduels/managers/player/PlayerRollBackManager.java
@@ -2,7 +2,6 @@ package us.polarismc.polarisduels.managers.player;
 
 import org.bukkit.*;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import us.polarismc.polarisduels.Main;
 
 public class PlayerRollBackManager {
@@ -17,18 +16,9 @@ public class PlayerRollBackManager {
         duelsPlayer.setDuel(false);
         plugin.tabManager.resetTabList(player);
 
-        player.setGameMode(GameMode.SURVIVAL);
-        player.getInventory().clear();
-        player.setFoodLevel(20);
-        player.setLevel(0);
-        player.getActivePotionEffects().forEach(potionEffect -> player.removePotionEffect(potionEffect.getType()));
-        player.setSaturation(5.0f);
-        player.setItemOnCursor(new ItemStack(Material.AIR));
+        plugin.hubManager.resetPlayerState(player);
         plugin.hubManager.giveLobbyItems(player);
-
-        World world = Bukkit.getWorld("lobby");
-        Location loc = new Location(world, 0, 100, 0);
-        player.teleport(loc);
+        plugin.hubManager.teleportToLobby(player);
 
         plugin.utils.delay(() -> player.setFireTicks(0));
     }
